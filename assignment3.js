1fff2aabe42c9397d073828ced4131c0c9fcf5db
Anita <anitamojolaoluwa@gmail.com>
2025-10-11 11:03:42 -0800

    my assignment 3

--- assignment3.js
+++ assignment3.js
@@ -1,13 +1,13 @@
 const side1 = 5
 
-const side2 = 5
+const side2 = 4
 
-const side3 = 5
+const side3 = 3
 
 if(side1 === side2 && side2 === side3){
     console.log("Equilateral triangle")
 }
-else{
+else if(side1 === side2 || side1 === side3 || side2 === side3){
     console.log("Isosceles triangle")
 }
 else{
