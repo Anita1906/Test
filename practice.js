af809651b38c808da82599a16f69b39c63240b75
Anita <anitamojolaoluwa@gmail.com>
2025-11-01 16:39:52 -0800

    Practice

---
+++ practice.js
@@ -0,0 +1,11 @@
+const day = "Thursday"
+
+if(day === "Friday") {
+    console.log("Thank God its Friday")
+} else if(day === "Saturday") {
+    console.log("It is weekend")
+} else if(day === "Sunday") {
+    console.log("Let's go to church")
+} else {
+    console.log("Go to work")
+}
